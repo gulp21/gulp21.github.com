2ed37c080d383d78914905ac3dc01cfef5182bf7
gulp21 <support.gulp21@googlemail.com>
2011-11-11 18:45:44 +0100

    * added screenshot + Linux 32 bit download * added "test with OS xy"

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ if(navigator.language.indexOf("de")>-1 && window.location.href.indexOf("index_de
 	window.location="index_de.html";
 }
 if(navigator.platform.toLowerCase().indexOf("linux")>-1) {
-	document.getElementById("trLinux").className="highlight";
+	if(navigator.platform.toLowerCase().indexOf("x86_64")>-1) {
+		document.getElementById("trLinux64").className="highlight";
+	} else if(navigator.platform.toLowerCase().indexOf("i686")>-1) {
+		document.getElementById("trLinux32").className="highlight";
+	} else {
+		document.getElementById("trLinux64").className="highlight";
+		document.getElementById("trLinux32").className="highlight";
+	}
 } else if(navigator.platform.toLowerCase().indexOf("win")>-1) {
 	document.getElementById("trWindows").className="highlight";
 }
